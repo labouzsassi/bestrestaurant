6a5057f172fea78dee74d8f7a47e99ce3d1d576a
labouzsassi <108271483+labouzsassi@users.noreply.github.com>
2022-06-28 11:32:24 +0100

    projet 1
    projet 1

---
+++ assets/js/animation.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $(".available").hide().show(3000);
+    $(".cc-menu").hide().fadeIn(10000);
+    $(".cc-carrousel").hide().slideDown(15000);
+    $("#passerunecommande").hide().fadeIn(20000);
+    $("footer").hide().show(25000);
+});
